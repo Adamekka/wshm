5bb097aa9f5a75bdf42e274845d43de76899f029
Adam Cvikl <adam.wasd4@gmail.com>
2023-09-08 17:57:15 +0200

    add lock, implement buttons

--- src/ui.rs
+++ src/ui.rs
@@ -24,13 +24,51 @@ impl BuildUI for &gtk4::Application {
 
         let label = gtk4::Label::new(Some("Shutdown Menu"));
 
+        let lock_button = gtk4::Button::with_label("Lock");
         let sleep_button = gtk4::Button::with_label("Sleep");
         let restart_button = gtk4::Button::with_label("Restart");
         let shutdown_button = gtk4::Button::with_label("Shutdown");
         let logout_button = gtk4::Button::with_label("Logout");
 
+        lock_button.connect_clicked(|_| {
+            std::process::Command::new("loginctl")
+                .arg("lock-session")
+                .spawn()
+                .expect("Failed to lock");
+        });
+
+        sleep_button.connect_clicked(|_| {
+            std::process::Command::new("systemctl")
+                .arg("suspend")
+                .spawn()
+                .expect("Failed to suspend");
+        });
+
+        restart_button.connect_clicked(|_| {
+            std::process::Command::new("systemctl")
+                .arg("reboot")
+                .spawn()
+                .expect("Failed to reboot");
+        });
+
+        shutdown_button.connect_clicked(|_| {
+            std::process::Command::new("shutdown")
+                .arg("now")
+                .spawn()
+                .expect("Failed to poweroff");
+        });
+
+        logout_button.connect_clicked(|_| {
+            std::process::Command::new("loginctl")
+                .arg("terminate-user")
+                .arg("1000")
+                .spawn()
+                .expect("Failed to logout");
+        });
+
         let widgets = gtk4::Box::new(gtk4::Orientation::Vertical, 0);
         widgets.append(&label);
+        widgets.append(&lock_button);
         widgets.append(&sleep_button);
         widgets.append(&restart_button);
         widgets.append(&shutdown_button);
